178c0b278afe9a9d012e8759cf535c38303b1765
pankajbinwal001 <pankajbinwal38@gmail.com>
2024-03-04 21:03:28 +0530

    updated cors

--- src/main/java/com/codebook/api/config/SecurityConfig.java
+++ src/main/java/com/codebook/api/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.codebook.api.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,15 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.codebook.api.jwt.JwtAuthenticationFilter;
-import com.codebook.api.user.service.CustomUserDetailsService;
 
-import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfig {
@@ -40,16 +32,7 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			public void addCorsMappings(CorsRegistry registry) {
-				 registry.addMapping("/**")
-				 .allowedMethods("*")
-				 .allowedOrigins("https://pankaj-code.netlify.app");
-			}
-		};
-	}
+	
 
 	@Bean
 	public DaoAuthenticationProvider daoAuthenticationProvider() {
@@ -67,26 +50,27 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityConfiguration(HttpSecurity http) throws Exception {
 		http.csrf(c -> c.disable())
+		.cors(Customizer.withDefaults())
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/orders/**").authenticated()
 						.requestMatchers("/users/**").authenticated()
 						.anyRequest().permitAll())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
-		http.cors(c -> c.configurationSource(new CorsConfigurationSource() {
-
-			@Override
-			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-				CorsConfiguration cfg = new CorsConfiguration();
-				cfg.setAllowedOrigins(Arrays.asList("https://pankaj-code.netlify.app","https://binni-codebook.netlify.app/**", "https://binni-codebook.netlify.app"));
-				cfg.setAllowedMethods(Collections.singletonList("*"));
-				cfg.setAllowCredentials(true);
-				cfg.setAllowedHeaders(Collections.singletonList("*"));
-				cfg.setExposedHeaders(Arrays.asList("Authorization","Access-Control-Allow-Origin"));
-				cfg.setMaxAge(3600L);
-				return cfg;
-			}
-		}));
+//		http.cors(c -> c.configurationSource(new CorsConfigurationSource() {
+//
+//			@Override
+//			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//				CorsConfiguration cfg = new CorsConfiguration();
+//				cfg.setAllowedOrigins(Arrays.asList("https://pankaj-code.netlify.app","https://binni-codebook.netlify.app/**", "https://binni-codebook.netlify.app"));
+//				cfg.setAllowedMethods(Collections.singletonList("*"));
+//				cfg.setAllowCredentials(true);
+//				cfg.setAllowedHeaders(Collections.singletonList("*"));
+//				cfg.setExposedHeaders(Arrays.asList("Authorization","Access-Control-Allow-Origin"));
+//				cfg.setMaxAge(3600L);
+//				return cfg;
+//			}
+//		}));
 		return http.build();
 	}
 }
